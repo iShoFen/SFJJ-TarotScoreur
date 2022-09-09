4e082c2be3580cc2ebe731f753e0edb4abfdc7c0
Jordan Artzet <jordan.artzet1@gmail.com>
2022-09-09 23:52:13 +0200

    Ajout de la solution
    Ajout d'un projet model qui contiendra le modèle de l'application
    
    Ajout d'un projet de Test Unitaire xUnit
    qui contiendra tout les test unitaires de nos différentes classes du modèle

---
+++ src/TarotScoreur/Model/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Player
+    {
+        public long id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string NickName { get; private set; }
+
+        public Player(long id, string firstName, string lastName, string nickName)
+        {
+            this.id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            NickName = nickName;
+        }
+    }
+}
---
+++ src/TarotScoreur/UT_Model/UT_Player.cs
@@ -0,0 +1,24 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT_Model
+{
+    public class UT_Player
+    {
+        [Fact]
+        public void TestConstructor()
+        {
+            Player player = new Player(1, "Jordan", "Artzet", "Firologia");
+            Assert.NotNull(player);
+            Assert.Equal(1,player.id);
+            Assert.Equal("Jordan", player.FirstName);
+            Assert.Equal("Artzet", player.LastName);
+            Assert.Equal("Firologia", player.NickName);
+
+        }
+    }
+}
