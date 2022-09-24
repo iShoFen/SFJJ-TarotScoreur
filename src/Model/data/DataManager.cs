568827018246d576a5ef4a5783a5b56fa8f6b72e
Julien THEME <julien.theme@etu.uca.fr>
2022-09-25 00:37:42 +0200

    :sparkles: add structure for persistence
    - add ISave interface 
    - add ILoad interface
    - add DataManager

---
+++ src/Model/data/DataManager.cs
@@ -0,0 +1,207 @@
+using Model.enums;
+
+namespace Model.data;
+
+public class PersistanceManager
+{
+    private ILoad iLoad;
+    private ISave iSave;
+    
+    /// <summary>
+    /// Constructor for the PersistanceManager
+    /// </summary>
+    /// <param name="iLoad">Interface to load data</param>
+    /// <param name="iSave">Interface to save data</param>
+    public PersistanceManager(ILoad iLoad , ISave iSave)
+    {
+        this.iLoad = iLoad;
+        this.iSave = iSave;
+    }
+
+    /*========== Players ==========*/
+    /// <summary>
+    /// Method to save a player
+    /// </summary>
+    /// <param name="player">Player to register</param>
+    public void SavePlayer(Player player) => iSave.SavePlayer(player);
+    
+    /// <summary>
+    /// Method to load all players
+    /// </summary>
+    /// <returns>List of players</returns>
+    public List<Player> LoadAllPlayer() => iLoad.LoadAllPlayer();
+    
+    /// <summary>
+    /// Method to load a player by group
+    /// </summary>
+    /// <param name="group">Group to search</param>
+    /// <returns>List of players</returns>
+    public List<Player> LoadPlayersByGroup(Group group) => iLoad.LoadPlayersByGroup(group);
+    
+    /// <summary>
+    /// Method to load a player by firstname
+    /// </summary>
+    /// <param name="firstName">Firstname to search</param>
+    /// <returns>List of players</returns>
+    public List<Player> LoadPlayerByFirstName(string firstName) => iLoad.LoadPlayerByFirstName(firstName);
+    
+    /// <summary>
+    /// Method to load a player by lastname
+    /// </summary>
+    /// <param name="lastName">Lastname to search</param>
+    /// <returns>List of players</returns>
+    public List<Player> LoadPlayerByLastName(string lastName) => iLoad.LoadPlayerByLastName(lastName);
+    
+    /// <summary>
+    /// Method to load a player by nickname
+    /// </summary>
+    /// <param name="nickName">Nickname to search</param>
+    /// <returns>List of players</returns>
+    public List<Player> LoadPlayerByNickName(string nickName) => iLoad.LoadPlayerByNickName(nickName);
+    
+    /// <summary>
+    /// Method to load a player by firstname and lastname
+    /// </summary>
+    /// <param name="firstName">Firstname to search</param>
+    /// <param name="lastName">Lastname to search</param>
+    /// <returns>List of players</returns>
+    public List<Player> LoadPlayerByFirstNameAndLastName(string firstName, string lastName) => iLoad.LoadPlayerByFirstNameAndLastName(firstName, lastName);
+    
+    /// <summary>
+    /// Method to load a player by firtsname and nickname
+    /// </summary>
+    /// <param name="firstName">Firstname to search</param>
+    /// <param name="nickname">Nickname to search</param>
+    /// <returns>List of players</returns>
+    public List<Player> LoadPlayerByFirstNameAndNickname(string firstName, string nickName) => iLoad.LoadPlayerByFirstNameAndNickname(firstName, nickName);
+    
+    /// <summary>
+    /// Method to load a player by lastname and nickname
+    /// </summary>
+    /// <param name="lastName">Lastname to search</param>
+    /// <param name="nickname">Nickname to search</param>
+    /// <returns>List of players</returns>
+    public List<Player> LoadPlayerByLastNameAndNickname(string lastName, string nickName) => iLoad.LoadPlayerByLastNameAndNickname(lastName, nickName);
+    /*========== End Players ==========*/
+    
+    
+    /*========== Games ==========*/
+    /// <summary>
+    /// Method to save a game
+    /// </summary>
+    /// <param name="game">Game to register</param>
+    public void SaveGame(Game game) => iSave.SaveGame(game);
+
+    /// <summary>
+    /// Method to load a game by name
+    /// </summary>
+    /// <param name="name">Name of the game</param>
+    /// <returns>A game</returns>
+    public Game LoadGameByName(string name) => iLoad.LoadGameByName(name);
+
+    /// <summary>
+    /// Method to load games by start date
+    /// </summary>
+    /// <param name="startDate">Start date of games</param>
+    /// <returns>List of games</returns>
+    public List<Game> LoadGameByStartDate(DateTime startDate) => iLoad.LoadGameByStartDate(startDate);
+    
+    /// <summary>
+    /// Method to load games by end date
+    /// </summary>
+    /// <param name="endDate">End date of games</param>
+    /// <returns>List of games</returns>
+    public List<Game> LoadGameByEndDate(DateTime endDate) => iLoad.LoadGameByEndDate(endDate);
+    
+    /// <summary>
+    /// Method to load games by an interval of dates
+    /// </summary>
+    /// <param name="startDate">Start date of the interval</param>
+    /// <param name="endDate">End date of the interval</param>
+    /// <returns>List of games</returns>
+    public List<Game> LoadGameByDateInterval(DateTime startDate, DateTime endDate) => iLoad.LoadGameByDateInterval(startDate, endDate);
+    
+    /// <summary>
+    /// Method to load games by an interval of dates and a group
+    /// </summary>
+    /// <param name="startDate">Start date of the interval</param>
+    /// <param name="endDate">End date of the interval</param>
+    /// <param name="group">Group to search</param>
+    /// <returns>List of games</returns>
+    public List<Game> LoadGameByDateAndGroupInterval(DateTime startDate, DateTime endDate, Group group) => iLoad.LoadGameByDateAndGroupInterval(startDate, endDate, group);
+
+    /// <summary>
+    /// Method to load games by an interval of dates and a player
+    /// </summary>
+    /// <param name="startDate">Start date of the interval</param>
+    /// <param name="endDate">End date of the interval</param>
+    /// <param name="player">Player to search</param>
+    /// <returns>List of games</returns>
+    public List<Game> LoadGameByDateAndPlayerInterval(DateTime startDate, DateTime endDate, Player player) => iLoad.LoadGameByDateAndPlayerInterval(startDate, endDate, player);
+    
+    /// <summary>
+    /// Method to load games by player
+    /// </summary>
+    /// <param name="player">Player to search</param>
+    /// <returns>List of games</returns>
+    public List<Game> LoadGameByPlayer(Player player) => iLoad.LoadGameByPlayer(player);
+    
+    /// <summary>
+    /// Method to load games by a group
+    /// </summary>
+    /// <param name="group">Group to search</param>
+    /// <returns>List of games</returns>
+    public List<Game> LoadGameByGroup(Group group) => iLoad.LoadGameByGroup(group);
+    
+    /// <summary>
+    /// Method to load all games
+    /// </summary>
+    /// <returns>List of games</returns>
+    public List<Game> LoadAllGames() => iLoad.LoadAllGames();
+    /*========== End Games ==========*/
+    
+    
+    /*========== Groups ==========*/
+    /// <summary>
+    /// Method to save a group
+    /// </summary>
+    /// <param name="group">Group to register</param>
+    public void SaveGroup(Group group) => iSave.SaveGroup(group);
+    
+    /// <summary>
+    /// Method to load a group by name
+    /// </summary>
+    /// <param name="name">Name to search</param>
+    /// <returns>A group</returns>
+    public Group LoadGroupsByName(string name) => iLoad.LoadGroupsByName(name);
+    
+    /// <summary>
+    /// Method to load all groups
+    /// </summary>
+    /// <returns>List of groups</returns>
+    public List<Group> LoadAllGroups() => iLoad.LoadAllGroups();
+    
+    /// <summary>
+    /// Method to load a group by player
+    /// </summary>
+    /// <param name="player">Player to search</param>
+    /// <returns>List of groups</returns>
+    public List<Group> LoadGroupsByPlayer(Player player) => iLoad.LoadGroupsByPlayer(player);
+    /*========== End Groups ==========*/
+    
+    
+    /*========== Rules ==========*/
+    /// <summary>
+    /// Method to load a rule by name
+    /// </summary>
+    /// <param name="name">Name of the rule to search</param>
+    /// <returns>A IRules</returns>
+    public IRules LoadRule(string name) => iLoad.LoadRule(name);
+    
+    /// <summary>
+    /// Method to load all rules
+    /// </summary>
+    /// <returns>List of rules</returns>
+    public List<IRules> LoadAllRules() =>iLoad.LoadAllRules();
+    /*========== End Rules ==========*/
+}
---
+++ src/Model/data/ILoad.cs
@@ -0,0 +1,175 @@
+using Model.enums;
+
+namespace Model.data;
+
+public interface ILoad
+{
+    /*========== Games ==========*/
+    /// <summary>
+    /// Method to load a game by name
+    /// </summary>
+    /// <param name="name">Name of the game</param>
+    /// <returns>A game</returns>
+    Game LoadGameByName(string name);
+    
+    /// <summary>
+    /// Method to load games by player
+    /// </summary>
+    /// <param name="player">Player to search</param>
+    /// <returns>List of games</returns>
+    List<Game> LoadGameByPlayer(Player player);
+    
+    /// <summary>
+    /// Method to load games by start date
+    /// </summary>
+    /// <param name="startDate">Start date of games</param>
+    /// <returns>List of games</returns>
+    List<Game> LoadGameByStartDate(DateTime startDate);
+    
+    /// <summary>
+    /// Method to load games by end date
+    /// </summary>
+    /// <param name="endDate">End date of games</param>
+    /// <returns>List of games</returns>
+    List<Game> LoadGameByEndDate(DateTime endDate);
+    
+    /// <summary>
+    /// Method to load games by an interval of dates
+    /// </summary>
+    /// <param name="startDate">Start date of the interval</param>
+    /// <param name="endDate">End date of the interval</param>
+    /// <returns>List of games</returns>
+    List<Game> LoadGameByDateInterval(DateTime startDate, DateTime endDate);
+    
+    /// <summary>
+    /// Method to load games by an interval of dates and a group
+    /// </summary>
+    /// <param name="startDate">Start date of the interval</param>
+    /// <param name="endDate">End date of the interval</param>
+    /// <param name="group">Group to search</param>
+    /// <returns>List of games</returns>
+    List<Game> LoadGameByDateAndGroupInterval(DateTime startDate, DateTime endDate,Group group);
+    
+    /// <summary>
+    /// Method to load games by an interval of dates and a player
+    /// </summary>
+    /// <param name="startDate">Start date of the interval</param>
+    /// <param name="endDate">End date of the interval</param>
+    /// <param name="player">Player to search</param>
+    /// <returns>List of games</returns>
+    List<Game> LoadGameByDateAndPlayerInterval(DateTime startDate, DateTime endDate,Player player);
+    
+    /// <summary>
+    /// Method to load games by a group
+    /// </summary>
+    /// <param name="group">Group to search</param>
+    /// <returns>List of games</returns>
+    List<Game> LoadGameByGroup(Group group);
+    
+    /// <summary>
+    /// Method to load all games
+    /// </summary>
+    /// <returns>List of games</returns>
+    List<Game> LoadAllGames();
+    /*========== End Games ==========*/
+    
+    
+    /*========== Players ==========*/
+    /// <summary>
+    /// Method to load a player by lastname and nickname
+    /// </summary>
+    /// <param name="lastName">Lastname to search</param>
+    /// <param name="nickname">Nickname to search</param>
+    /// <returns>List of players</returns>
+    List<Player> LoadPlayerByLastNameAndNickname(string lastName, string nickname);
+    
+    /// <summary>
+    /// Method to load a player by firtsname and nickname
+    /// </summary>
+    /// <param name="firstName">Firstname to search</param>
+    /// <param name="nickname">Nickname to search</param>
+    /// <returns>List of players</returns>
+    List<Player> LoadPlayerByFirstNameAndNickname(string firstName, string nickname);
+    
+    /// <summary>
+    /// Method to load a player by firstname and lastname
+    /// </summary>
+    /// <param name="firstName">Firstname to search</param>
+    /// <param name="lastName">Lastname to search</param>
+    /// <returns>List of players</returns>
+    List<Player> LoadPlayerByFirstNameAndLastName(string firstName, string lastName);
+    
+    /// <summary>
+    /// Method to load a player by nickname
+    /// </summary>
+    /// <param name="nickName">Nickname to search</param>
+    /// <returns>List of players</returns>
+    List<Player> LoadPlayerByNickName(string nickName);
+    
+    /// <summary>
+    /// Method to load a player by lastname
+    /// </summary>
+    /// <param name="lastName">Lastname to search</param>
+    /// <returns>List of players</returns>
+    List<Player> LoadPlayerByLastName(string lastName);
+    
+    /// <summary>
+    /// Method to load a player by firstname
+    /// </summary>
+    /// <param name="firstName">Firstname to search</param>
+    /// <returns>List of players</returns>
+    List<Player> LoadPlayerByFirstName(string firstName);
+    
+    /// <summary>
+    /// Method to load all players
+    /// </summary>
+    /// <returns>List of players</returns>
+    List<Player> LoadAllPlayer();
+    
+    /// <summary>
+    /// Method to load a player by group
+    /// </summary>
+    /// <param name="group">Group to search</param>
+    /// <returns>List of players</returns>
+    List<Player> LoadPlayersByGroup(Group group);
+    /*========== End Players ==========*/
+    
+    
+    /*========== Groups ==========*/
+    /// <summary>
+    /// Method to load a group by name
+    /// </summary>
+    /// <param name="name">Name to search</param>
+    /// <returns>A group</returns>
+    Group LoadGroupsByName(string name);
+    
+    /// <summary>
+    /// Method to load all groups
+    /// </summary>
+    /// <returns>List of groups</returns>
+    List<Group> LoadAllGroups();
+    
+    /// <summary>
+    /// Method to load a group by player
+    /// </summary>
+    /// <param name="player">Player to search</param>
+    /// <returns>List of groups</returns>
+    List<Group> LoadGroupsByPlayer(Player player);
+    /*========== End Groups ==========*/
+    
+    
+    /*========== Rules ==========*/
+    /// <summary>
+    /// Method to load a rule by name
+    /// </summary>
+    /// <param name="name">Name of the rule to search</param>
+    /// <returns>A IRules</returns>
+    IRules LoadRule(string name);
+    
+    /// <summary>
+    /// Method to load all rules
+    /// </summary>
+    /// <returns>List of rules</returns>
+    List<IRules> LoadAllRules();
+    /*========== End Rules ==========*/
+}
