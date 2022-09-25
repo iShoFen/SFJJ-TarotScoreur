d850504fede9b791914dce231823bb9e7bfdad36
Florent MARQUES <80977545+flomSStaar@users.noreply.github.com>
2022-09-26 00:53:10 +0200

    :sparkles: add id and constructor in group
    Ajout d'un attribut identifiant et d'un constructeur pour ajouter les joueurs à la construction

--- src/Model/Group.cs
+++ src/Model/Group.cs
@@ -5,6 +5,11 @@ namespace Model;
 public class Group
 {
     /// <summary>
+    /// id of the Group
+    /// </summary>
+    public ulong Id { get; }
+
+    /// <summary>
     /// name of the Group
     /// </summary>
     public string Name { get; }
@@ -19,9 +24,27 @@ public class Group
     /// constructor
     /// </summary>
     /// <param name="name">name of the Group</param>
-    public Group(string name)
+    /// <param name="players">players to add in the Group</param>
+    public Group(string name, params Player[] players)
+        : this(0, name, players)
+    { }
+
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="id">id of the Group</param>
+    /// <param name="name">name of the Group</param>
+    /// <param name="players">players to add in the Group</param>
+    public Group(ulong id, string name, params Player[] players)
     {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentException("the group must have a name");
+        }
+
+        Id = id;
         Name = name;
+        AddPlayers(players);
         Players = new ReadOnlyCollection<Player>(_players);
     }
 
@@ -30,22 +53,37 @@ public class Group
     /// </summary>
     /// <param name="player">player to add</param>
     /// <exception cref="ArgumentException">if the player is null</exception>
-    public void AddPlayer(Player player)
+    public bool AddPlayer(Player player)
     {
         if (player == null)
         {
             throw new ArgumentException("Cannot add player because it is null");
         }
+        if (_players.Contains(player))
+        {
+            return false;
+        }
         _players.Add(player);
+        return true;
+    }
+
+    /// <summary>
+    /// adds a range of Player in this Group
+    /// </summary>
+    /// <param name="players">players to add</param>
+    public bool AddPlayers(params Player[] players)
+    {
+        var canAllBeAdded = players.Any(player => _players.Contains(player));
+        return !canAllBeAdded && players.All(AddPlayer);
     }
 
     /// <summary>
     /// removes a player in this Group
     /// </summary>
     /// <param name="player">player to remove</param>
-    public void RemovePlayer(Player player)
+    public bool RemovePlayer(Player player)
     {
-        _players.Remove(player);
+        return _players.Remove(player);
     }
 
     /// <summary>
