820a1f116f5fb83b9542d5664b6167718bb5f4e1
Florent MARQUES <80977545+flomSStaar@users.noreply.github.com>
2022-09-26 00:53:24 +0200

    :white_check_mark: add tests for group

---
+++ src/Tests/UT_Model/GroupTestData.cs
@@ -0,0 +1,246 @@
+﻿using Model;
+
+namespace UT_Model;
+
+class GroupTestData
+{
+    public static IEnumerable<object?[]> Data_TestAddPlayer()
+    {
+        yield return new object?[]
+        {
+            true,
+            new Group("partie",
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")),
+            new Player("Jordan", "Artzet", "Jo", "avatar"),
+            new Player[]
+            {
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar"),
+                new("Jordan", "Artzet", "Jo", "avatar")
+            }
+        };
+        yield return new object?[]
+        {
+            true,
+            new Group("partie"),
+            new Player("Jordan", "Artzet", "Jo", "avatar"),
+            new Player[]
+            {
+                new("Jordan", "Artzet", "Jo", "avatar")
+            }
+        };
+        yield return new object?[]
+{
+            false,
+            new Group("partie",
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar"),
+                new("Jordan", "Artzet", "Jo", "avatar")),
+            new Player("Jordan", "Artzet", "Jo", "avatar"),
+            new Player[]
+            {
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar"),
+                new("Jordan", "Artzet", "Jo", "avatar")
+            }
+        };
+    }
+
+    public static IEnumerable<object?[]> Data_TestAddPlayers()
+    {
+        yield return new object?[]
+        {
+            true,
+            new Group("partie",
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")),
+            new Player[]
+            {
+                new Player("Florent", "Marques", "Flo", "avatar"),
+                new Player("Jordan", "Artzet", "Jo", "avatar"),
+            },
+            new Player[]
+            {
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar"),
+                new("Jordan", "Artzet", "Jo", "avatar")
+            }
+        };
+        yield return new object?[]
+        {
+            true,
+            new Group("partie"),
+            new Player[]
+            {
+                new Player("Florent", "Marques", "Flo", "avatar"),
+                new Player("Jordan", "Artzet", "Jo", "avatar"),
+            },
+            new Player[]
+            {
+                new Player("Florent", "Marques", "Flo", "avatar"),
+                new Player("Jordan", "Artzet", "Jo", "avatar"),
+            }
+        };
+        yield return new object?[]
+{
+            true,
+            new Group("partie"),
+            Array.Empty<Player>(),
+            Array.Empty<Player>()
+        };
+        yield return new object?[]
+{
+            true,
+            new Group("partie"),
+            new Player[]
+            {
+                new Player("Florent", "Marques", "Flo", "avatar")
+            },
+            new Player[]
+            {
+                new Player("Florent", "Marques", "Flo", "avatar")
+            }
+        };
+        yield return new object?[]
+        {
+            true,
+            new Group("partie",
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")),
+            Array.Empty<Player>(),
+            new Player[]
+            {
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")
+            }
+        };
+        yield return new object?[]
+        {
+            false,
+            new Group("partie"),
+            new Player[]
+            {
+                new("Julien", "Theme", "Ju", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")
+            },
+            new Player[]
+            {
+                new("Julien", "Theme", "Ju", "avatar")
+            }
+        };
+        yield return new object?[]
+        {
+            false,
+            new Group("partie",
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")),
+            new Player[]
+            {
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")
+            },
+            new Player[]
+            {
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")
+            }
+        };
+        yield return new object?[]
+        {
+            false,
+            new Group("partie",
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")),
+            new Player[]
+            {
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")
+            },
+            new Player[]
+            {
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")
+            }
+        };
+    }
+
+    public static IEnumerable<object?[]> Data_TestRemovePlayer()
+    {
+        yield return new object?[]
+        {
+            false,
+            new Group("partie",
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")),
+            new Player("Jordan", "Artzet", "Jo", "avatar"),
+            new Player[]
+            {
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")
+            }
+        };
+        yield return new object?[]
+        {
+            false,
+            new Group("partie"),
+            new Player("Jordan", "Artzet", "Jo", "avatar"),
+            Array.Empty<Player>()
+        };
+        yield return new object?[]
+{
+            true,
+            new Group("partie",
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar"),
+                new("Jordan", "Artzet", "Jo", "avatar")),
+            new Player("Jordan", "Artzet", "Jo", "avatar"),
+            new Player[]
+            {
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar")
+            }
+        };
+        yield return new object?[]
+{
+            true,
+            new Group("partie",new Player("Jordan", "Artzet", "Jo", "avatar")),
+            new Player("Jordan", "Artzet", "Jo", "avatar"),
+            Array.Empty<Player>()
+        };
+    }
+
+    public static IEnumerable<object?[]> Data_TestClearPlayers()
+    {
+        yield return new object?[]
+        {
+            new Group("partie",
+                new("Florent", "Marques", "Flo", "avatar"),
+                new("Samuel", "Sirven", "Sam", "avatar"),
+                new("Julien", "Theme", "Ju", "avatar"),
+                new("Jordan", "Artzet", "Jo", "avatar")
+            )
+        };
+        yield return new object?[]
+        {
+            new Group("partie",
+                new Player("Florent", "Marques", "Flo", "avatar"))
+        };
+
+        yield return new object?[]
+        {
+            new Group("partie")
+        };
+    }
+
+
+}
---
+++ src/Tests/UT_Model/UT_Group.cs
@@ -0,0 +1,71 @@
+﻿using Model;
+using Xunit;
+
+namespace UT_Model;
+
+public class UT_Group
+{
+    [Theory]
+    [InlineData(true, 3, "partie", 3, "partie")]
+    [InlineData(true, 0, "partie", 0, "partie")]
+    [InlineData(false, 3, "    ", 3, "")]
+    [InlineData(false, 3, "", 3, "")]
+    [InlineData(false, 3, null, 3, "")]
+    public void TestFullConstructor(bool isValid, ulong id, string name, ulong expectedId, string expectedName)
+    {
+        if (isValid)
+        {
+            Group group = new(id, name);
+            Assert.Equal(expectedId, group.Id);
+            Assert.Equal(expectedName, group.Name);
+            Assert.Empty(group.Players);
+        }
+        else
+        {
+            Assert.ThrowsAny<ArgumentException>(() => new Group(id, name));
+        }
+    }
+
+    [Fact]
+    public void TestConstructor()
+    {
+        Group group = new("partie");
+        ulong expectedId = 0;
+
+        Assert.Equal(expectedId, group.Id);
+        Assert.Equal("partie", group.Name);
+        Assert.Empty(group.Players);
+    }
+
+    [Theory]
+    [MemberData(nameof(GroupTestData.Data_TestAddPlayer), MemberType = typeof(GroupTestData))]
+    public void TestAddPlayer(bool isValid, Group group, Player player, IEnumerable<Player> expectedPlayers)
+    {
+        Assert.Equal(isValid, group.AddPlayer(player));
+        Assert.Equal(expectedPlayers, group.Players);
+    }
+
+    [Fact]
+    public void TestAddNullPlayer()
+    {
+        Group group = new("partie");
+
+        Assert.ThrowsAny<ArgumentException>(() => group.AddPlayer(null!));
+    }
+
+    [Theory]
+    [MemberData(nameof(GroupTestData.Data_TestRemovePlayer), MemberType = typeof(GroupTestData))]
+    public void TestRemovePlayer(bool isValid, Group group, Player player, IEnumerable<Player> expectedPlayers)
+    {
+        Assert.Equal(isValid, group.RemovePlayer(player));
+        Assert.Equal(expectedPlayers, group.Players);
+    }
+
+    [Theory]
+    [MemberData(nameof(GroupTestData.Data_TestClearPlayers), MemberType = typeof(GroupTestData))]
+    public void TestClearPlayers(Group group)
+    {
+        group.ClearPlayers();
+        Assert.Empty(group.Players);
+    }
+}
