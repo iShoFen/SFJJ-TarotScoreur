693e3d1137b4b0943c17ef6da89174d3c68dec46
Florent MARQUES <63flotoof@gmail.com>
2022-09-25 18:40:56 +0200

    :white_check_mark: add functional test

---
+++ src/Tests/FT_Model/FT_Ranking.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using Model.Ranking;
+
+var datas = new List<PlayerData>();
+for (int i = 0; i < 10; i++)
+{
+    var random = new Random();
+    var p = new Player("Toto" + i, "Titi" + i, "Tutu" + i, "avatar");
+    datas.Add(new PlayerData(p, random.Next(10000), random.Next(10000)));
+}
+
+var playerRanking = new PlayerRanking("Toto", GameType.FourPlayers, datas.ToArray());
+
+
+Console.WriteLine("Sort by ascending loss");
+foreach (var player in playerRanking.SortByAscendingLoss())
+{
+    Console.WriteLine($"{player.LossCount} - {player.Player}");
+}
+
+Console.WriteLine("---------------------------");
+
+Console.WriteLine("Sort by descending loss");
+foreach (var player in playerRanking.SortByDescendingLoss())
+{
+    Console.WriteLine($"{player.LossCount} - {player.Player}");
+}
