5e8b752348799c2fe4353f42eaffcc9379c7d321
Samuel SIRVEN <samuel.sirven@gmail.com>
2022-09-30 00:44:00 +0200

    :sparkles: add dbset of games

--- Sources/TarotDB/TarotDBContext.cs
+++ Sources/TarotDB/TarotDBContext.cs
@@ -4,6 +4,7 @@ namespace TarotDB;
 
 internal class TarotDBContext : DbContext
 {
+    public DbSet<GameEntity> Games { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlite("Data Source=TarotScoreur.db");
