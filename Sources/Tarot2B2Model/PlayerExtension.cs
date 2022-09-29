c5faceaf5a631a3aa64f6de114eb5fdf73b2d4f5
Florent MARQUES <80977545+flomSStaar@users.noreply.github.com>
2022-09-29 23:13:54 +0200

    :sparkles: add project for extension method
    Ajout d'un projet pour implémenter les méthodes permettant la transformation d'objet du modèle en objet de la base de données

---
+++ Sources/Tarot2B2Model/PlayerExtension.cs
@@ -0,0 +1,31 @@
+﻿using Model;
+using TarotDB;
+
+namespace Tarot2B2Model;
+
+static class PlayerExtension
+{
+    public static PlayerEntity ToEntity(this Player player)
+    {
+        return new PlayerEntity()
+        {
+            Id = player.Id,
+            FirstName = player.FirstName,
+            LastName = player.LastName,
+            Nickname = player.NickName,
+            Avatar = player.Avatar
+        };
+    }
+    
+    public static Player ToModel(this PlayerEntity playerEntity)
+    {
+        return new Player(playerEntity.Id, playerEntity.FirstName, playerEntity.LastName, playerEntity.Nickname,
+            playerEntity.Avatar);
+    }
+
+    public static IEnumerable<PlayerEntity> ToEntities(this IEnumerable<Player> entities)
+        => entities.Select(e => e.ToEntity());
+
+    public static IEnumerable<Player> ToEntities(this IEnumerable<PlayerEntity> entities)
+        => entities.Select(e => e.ToModel());
+}
