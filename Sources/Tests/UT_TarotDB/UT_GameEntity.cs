7abac941eae8f13115fac9a22838a89768d09fbc
Samuel SIRVEN <samuel.sirven@gmail.com>
2022-10-07 02:07:10 +0200

    :test_tube: not working test for stub context
    - Test not work cause of join table
    
    PS : join table have been commented for the sake of build.  Patch => WIP

--- Sources/Tests/UT_TarotDB/UT_GameEntity.cs
+++ Sources/Tests/UT_TarotDB/UT_GameEntity.cs
@@ -1,11 +1,39 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using StubContext;
+using TarotDB;
 using Xunit;
 
 namespace UT_TarotDB;
 
 public class UT_GameEntity
 {
+    private static DbContextOptions<TarotDBContext> InitDB()
+
+    {
+
+        //connection must be opened to use In-memory database
+
+        var connection = new SqliteConnection("DataSource=:memory:");
+
+        connection.Open();
+
+        var options = new DbContextOptionsBuilder<TarotDBContext>()
+
+            .UseSqlite(connection)
+
+            .Options;
+
+        return options;
+    }
+    
     [Fact]
     public void Test1()
     {
+        using (var context = new TarotDBContextStub(InitDB()))
+        {
+            var test = context.Database.EnsureCreated();
+            Assert.True(test);
+        }
     }
 }
