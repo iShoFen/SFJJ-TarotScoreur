a822cbb485358cc6f14f62662ab529ebfff2ec20
Jordan Artzet <jordan.artzet1@gmail.com>
2022-10-08 17:44:41 +0200

    :sparkles: add class DBLoader and DBSaver
    Classes ajouté :
    DBLoader : loader qui permet de charger les données en BD dans le model
    DBSaver :  saver qui permet de sauvegarder les données du model en BD
    
    Problème:
    Ne peut pas charger les rules car il n'y a pas de RulesEntity.
    
    Solution : Ajouter RulesEntity

---
+++ Sources/Tarot2B2Model/DBLoader.cs
@@ -0,0 +1,321 @@
+﻿using Model;
+using Model.data;
+using Model.games;
+using TarotDB;
+
+namespace Tarot2B2Model;
+
+public class DBLoader : ILoader
+{
+    public Game? LoadGameByName(string name)
+    {
+        Game? game = null;
+        using (var context = new TarotDBContext())
+        {
+            game = context.Games.Single(g => g.Name.Contains(name)).ToModel();
+        }
+
+        return game;
+    }
+
+    public IEnumerable<Game> LoadGameByPlayer(Player player, int page, int pageSize)
+    {
+        List<Game> games;
+        using (var context = new TarotDBContext())
+        {
+            games = context.Games.Where(g => g.Players.Any(p => p.ToModel().Equals(player)))
+                                 .Skip((page-1) * pageSize)
+                                 .Take(pageSize)
+                                 .Select(g => g.ToModel())
+                                 .ToList();
+        }
+        return games;
+    }
+
+    public IEnumerable<Game> LoadGameByStartDate(DateTime startDate, int page, int pageSize)
+    {
+        List<Game> games;
+        using (var context = new TarotDBContext())
+        {
+            games = context.Games.Where(g => g.StartDate == startDate)
+                                 .Skip((page-1) * pageSize)
+                                 .Take(pageSize)
+                                 .Select(g => g.ToModel())
+                                 .ToList();
+        }
+
+        return games;
+    }
+
+    public IEnumerable<Game> LoadGameByEndDate(DateTime endDate, int page, int pageSize)
+    {
+        List<Game> games;
+        using (var context = new TarotDBContext())
+        {
+            games = context.Games.Where(g => g.EndDate == endDate)
+                                 .Skip((page-1) * pageSize)
+                                 .Take(pageSize)
+                                 .Select(g => g.ToModel())
+                                 .ToList();
+        }
+
+        return games;
+    }
+
+    public IEnumerable<Game> LoadGameByDateInterval(DateTime startDate, DateTime endDate, int page, int pageSize)
+    {
+        List<Game> games;
+        using (var context = new TarotDBContext())
+        {
+            games = context.Games.Where(g => g.StartDate >= startDate && g.EndDate <= endDate)
+                                 .Skip((page-1) * pageSize)
+                                 .Take(pageSize)
+                                 .Select(g => g.ToModel())
+                                 .ToList();
+        }
+
+        return games;
+    }
+
+    public IEnumerable<Game> LoadGameByDateIntervalAndGroup(DateTime startDate, DateTime endDate, Group group, int page, int pageSize)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Game> LoadGameByDateIntervalAndPlayer(DateTime startDate, DateTime endDate, Player player, int page,
+        int pageSize)
+    {
+        List<Game> games;
+        using (var context = new TarotDBContext())
+        {
+            games = context.Games.Where(g => g.StartDate >= startDate && g.EndDate <= endDate && g.Players.Any(p => p.ToModel().Equals(player)))
+                                 .Skip((page-1) * pageSize)
+                                 .Take(pageSize)
+                                 .Select(g => g.ToModel())
+                                 .ToList();
+        }
+
+        return games;
+    }
+
+    public IEnumerable<Game> LoadGameByGroup(Group group, int page, int pageSize)
+    {
+        List<Game> games;
+        using (var context = new TarotDBContext())
+        {
+            games = context.Games.Where(g => g.Players.Any(p => group.Players.Contains(p.ToModel())))
+                                 .Skip((page-1) * pageSize)
+                                 .Take(pageSize)
+                                 .Select(g => g.ToModel())
+                                 .ToList();
+        }
+
+        return games;
+    }
+
+    public IEnumerable<Game> LoadAllGames(int page, int pageSize)
+    {
+        List<Game> games;
+        using (var context = new TarotDBContext())
+        {
+            games = context.Games.Skip((page-1) * pageSize)
+                                 .Take(pageSize)
+                                 .Select(g => g.ToModel())
+                                 .ToList();
+        }
+        return games;
+    }
+
+    public IEnumerable<Player> LoadPlayerByLastNameAndNickname(string lastName, string nickname, int page, int pageSize)
+    {
+        List<Player> players;
+        using (var context = new TarotDBContext())
+        {
+            players = context.Players.Where(p => p.LastName.Contains(lastName) && p.Nickname.Contains(nickname))
+                                     .Skip((page-1) * pageSize)
+                                     .Take(pageSize)
+                                     .Select(p => p.ToModel())
+                                     .ToList();
+        }
+
+        return players;
+    }
+
+    public IEnumerable<Player> LoadPlayerByFirstNameAndNickname(string firstName, string nickname, int page, int pageSize)
+    {
+        List<Player> players;
+        using (var context = new TarotDBContext())
+        {
+            players = context.Players.Where(p => p.FirstName.Contains(firstName) && p.Nickname.Contains(nickname))
+                                     .Skip((page-1) * pageSize)
+                                     .Take(pageSize)
+                                     .Select(p => p.ToModel())
+                                     .ToList();
+        }
+
+        return players;
+
+    }
+
+    public IEnumerable<Player> LoadPlayerByFirstNameAndLastName(string firstName, string lastName, int page, int pageSize)
+    {
+        List<Player> players;
+        using (var context = new TarotDBContext())
+        {
+            players = context.Players.Where(p => p.FirstName.Contains(firstName) && p.LastName.Contains(lastName))
+                                     .Skip((page-1) * pageSize)
+                                     .Take(pageSize)
+                                     .Select(p => p.ToModel())
+                                     .ToList();
+        }
+
+        return players;
+    }
+
+    public IEnumerable<Player> LoadPlayerByNickname(string nickname, int page, int pageSize)
+    {
+        List<Player> players;
+        using (var context = new TarotDBContext())
+        {
+            players = context.Players.Where(p => p.Nickname.Contains(nickname))
+                                     .Skip((page-1) * pageSize)
+                                     .Take(pageSize)
+                                     .Select(p => p.ToModel())
+                                     .ToList();
+        }
+
+        return players;
+    }
+
+    public IEnumerable<Player> LoadPlayerByLastName(string lastName, int page, int pageSize)
+    {
+        List<Player> players;
+        using (var context = new TarotDBContext())
+        {
+            players = context.Players.Where(p => p.LastName.Contains(lastName))
+                                     .Skip((page-1) * pageSize)
+                                     .Take(pageSize)
+                                     .Select(p => p.ToModel())
+                                     .ToList();
+        }
+
+        return players;
+    }
+
+    public IEnumerable<Player> LoadPlayerByFirstName(string firstName, int page, int pageSize)
+    {
+        List<Player> players;
+        using (var context = new TarotDBContext())
+        {
+            players = context.Players.Where(p => p.LastName.Contains(firstName))
+                .Skip((page-1) * pageSize)
+                .Take(pageSize)
+                .Select(p => p.ToModel())
+                .ToList();
+        }
+        return players;
+    }
+
+    public IEnumerable<Player> LoadAllPlayer(int page, int pageSize)
+    {
+        List<Player> players;
+        using (var context = new TarotDBContext())
+        {
+            players = context.Players.Skip((page-1) * pageSize)
+                                     .Take(pageSize)
+                                     .Select(p => p.ToModel())
+                                     .ToList();
+        }
+        return players;
+    }
+
+    public IEnumerable<Player> LoadPlayersByGroup(Group group, int page, int pageSize)
+    {
+        List<Player> players;
+        using (var context = new TarotDBContext())
+        {
+            players = context.Players.Where(p => group.Players.Contains(p.ToModel()))
+                                     .Skip((page-1) * pageSize)
+                                     .Take(pageSize)
+                                     .Select(p => p.ToModel())
+                                     .ToList();
+        }
+        return players;
+    }
+
+    public Group? LoadGroupsByName(string name)
+    {
+        Group group;
+        using (var context = new TarotDBContext())
+        {
+            group = context.Groups.FirstOrDefault(g => g.Name == name)?.ToModel();
+        }
+        return group;
+    }
+
+    public IEnumerable<Group> LoadAllGroups(int page, int pageSize)
+    {
+        List<Group> groups;
+        using (var context = new TarotDBContext())
+        {
+            groups = context.Groups.Skip((page-1) * pageSize)
+                                   .Take(pageSize)
+                                   .Select(g => g.ToModel())
+                                   .ToList();
+        }
+        return groups;
+    }
+
+    public IEnumerable<Group> LoadGroupsByPlayer(Player player, int page, int pageSize)
+    {
+        List<Group> groups;
+        using (var context = new TarotDBContext())
+        {
+            groups = context.Groups.Where(g => g.Players.Any(p => p.ToModel().Equals(player)))
+                                   .Skip((page-1) * pageSize)
+                                   .Take(pageSize)
+                                   .Select(g => g.ToModel())
+                                   .ToList();
+        }
+        return groups;
+    }
+
+    public IRules? LoadRule(string name)
+    {
+        // IRules rules;
+        // using (var context = new TarotDBContext())
+        // {
+        //     rules = context.Rules.FirstOrDefault(r => r.Name == name)?.ToModel();
+        // }
+        // return rules;
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<IRules> LoadAllRules(int page, int pageSize)
+    {
+        // List<IRules> rules;
+        // using (var context = new TarotDBContext())
+        // {
+        //     rules = context.Rules.Skip((page-1) * pageSize)
+        //                          .Take(pageSize)
+        //                          .Select(r => r.ToModel())
+        //                          .ToList();
+        // }
+        // return rules;
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<KeyValuePair<int, Hand>> LoadHandByGame(Game game, int page, int pageSize)
+    {
+        List<KeyValuePair<int, Hand>> hands;
+        using (var context = new TarotDBContext())
+        {
+            hands = context.Hands.Where(h => h.Game.ToModel().Equals(game))
+                                 .Skip((page-1) * pageSize)
+                                 .Take(pageSize)
+                                 .Select((h, i) => new KeyValuePair<int, Hand>(i, h.ToModel()))
+                                 .ToList();
+        }
+        return hands;
+    }
+}
---
+++ Sources/Tarot2B2Model/DBSaver.cs
@@ -0,0 +1,36 @@
+﻿using Model;
+using Model.data;
+using Model.games;
+using TarotDB;
+
+namespace Tarot2B2Model;
+
+public class DBSaver : ISaver
+{
+    public void SavePlayer(Player player)
+    {
+        using(var context = new TarotDBContext())
+        {
+            context.Players.Add(player.ToEntity());
+            context.SaveChanges();
+        }
+    }
+
+    public void SaveGame(Game game)
+    {
+        using(var context = new TarotDBContext())
+        {
+            context.Games.Add(game.ToEntity());
+            context.SaveChanges();
+        }
+    }
+
+    public void SaveGroup(Group group)
+    {
+        using(var context = new TarotDBContext())
+        {
+            context.Groups.Add(group.ToEntity());
+            context.SaveChanges();
+        }    
+    }
+}
