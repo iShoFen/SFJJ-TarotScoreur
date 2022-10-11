5077ea8a4a7d028567b15320d10f77f6a03f489d
Florent MARQUES <63flotoof@gmail.com>
2022-10-11 17:11:55 +0200

    🔀 merge branch 'origin/develop' -> 'test/ef/player/51'

---
+++ Sources/Tests/UT_TarotDB/GameEntityTestData.cs
@@ -0,0 +1,57 @@
+﻿namespace UT_TarotDB;
+
+public static class GameEntityTestData
+{
+    public static IEnumerable<object?[]> Data_TestRead()
+    {
+        yield return new object?[]
+        {
+            1UL,
+            "Game1",
+            "FrenchTarotRules",
+            new DateTime(2022, 09, 21),
+            null,
+            new[]
+            {
+                1UL, 2UL, 3UL
+            },
+            new[]
+            {
+                1UL, 2UL, 3UL
+            }
+        };
+        yield return new object?[]
+        {
+	        6UL,
+	        "Game6",
+	        "FrenchTarotRules",
+	        new DateTime(2022, 09, 21),
+	        new DateTime(2022, 09, 29),
+	        new[]
+	        {
+		        6UL, 7UL, 8UL, 9UL
+	        },
+	        new[]
+	        {
+		        16UL, 17UL, 18UL, 19UL
+	        }
+        };
+        yield return new object?[]
+        {
+	        9UL,
+	        "Game9",
+	        "FrenchTarotRules",
+	        new DateTime(2022, 09, 21),
+	        new DateTime(2022, 09, 30),
+	        new[]
+	        {
+		        9UL, 10UL, 11UL, 12UL, 13UL
+	        },
+	        new[]
+	        {
+		        24UL, 25UL, 26UL, 27UL, 28UL
+	        }
+        };
+    }
+    
+}
--- Sources/Tests/UT_TarotDB/UT_GameEntity.cs
+++ Sources/Tests/UT_TarotDB/UT_GameEntity.cs
@@ -26,14 +26,37 @@ public class UT_GameEntity
 
         return options;
     }
-    
-    [Fact]
-    public void Test1()
+
+    [Theory]
+    [MemberData(nameof(GameEntityTestData.Data_TestRead), MemberType = typeof(GameEntityTestData))]
+    public async Task TestRead(ulong expId, string expName, string expRules, DateTime expStartDate,
+	    DateTime? expEndDate, IEnumerable<ulong> expPlayers, IEnumerable<ulong> expHands)
     {
-        using (var context = new TarotDBContextStub(InitDB()))
-        {
-            var test = context.Database.EnsureCreated();
-            Assert.True(test);
-        }
+	    await using var context = new TarotDBContextStub(InitDB());
+	    await context.Database.EnsureCreatedAsync();
+	    
+	    var game = await context.Games.Include(ga => ga.Players)
+		    .Include(ga => ga.Hands).ThenInclude(ha => ha.Biddings)
+		    .SingleOrDefaultAsync(ga => ga.Id == expId);
+		    
+	    Assert.Equal(expId, game!.Id);
+	    Assert.Equal(expRules, game.Rules);
+	    Assert.Equal(expName, game.Name);
+	    Assert.Equal(expStartDate, game.StartDate);
+	    Assert.Equal(expEndDate, game.EndDate);
+
+	    foreach (var player in expPlayers)
+	    {
+		    Assert.Single(game.Players.Where(pl => pl.Id == player 
+		                                           && pl.Equals(context.Players.Find(player))
+		                                           && pl.Games.Contains(game)));
+	    }
+		    
+	    foreach (var hand in expHands)
+	    {
+		    Assert.Single(game.Hands.Where(ha => ha.Id == hand 
+		                                         && ha.Equals(context.Hands.Find(hand))
+		                                         && ha.Game.Equals(game)));
+	    }
     }
 }
