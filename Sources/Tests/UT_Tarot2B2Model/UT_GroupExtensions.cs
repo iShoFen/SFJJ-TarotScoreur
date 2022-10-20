9c30e153b78b2d7c7639c93498cd30044a9d40f8
Jordan Artzet <jordan.artzet1@gmail.com>
2022-10-21 00:59:12 +0200

    :bug: resolve a bug in tests
    GroupExtensions test are resolved.

--- Sources/Tests/UT_Tarot2B2Model/UT_GroupExtensions.cs
+++ Sources/Tests/UT_Tarot2B2Model/UT_GroupExtensions.cs
@@ -55,26 +55,116 @@ public class UT_GroupExtensions
         {
             new List<Group>()
             {
-                new Group(1UL, "Group1" ),
-                new Group(2UL, "Group2" ),
-                new Group(3UL, "Group3" ),
+                new Group(1UL, "Group1",
+                    new Player(0UL, "Jean", "BON", "JEBO", "avatar1"),
+                    new Player(1UL, "Pierre", "DURAND", "PIER", "avatar2"),
+                    new Player(2UL, "Paul", "MARTIN", "PAUL", "avatar3")),
+                new Group(2UL, "Group2",
+                    new Player(0UL, "Jean", "BON", "JEBO", "avatar1"),
+                    new Player(1UL, "Pierre", "DURAND", "PIER", "avatar2"),
+                    new Player(2UL, "Paul", "MARTIN", "PAUL", "avatar3")),
+                new Group(3UL, "Group3",
+                    new Player(0UL, "Jean", "BON", "JEBO", "avatar1"),
+                    new Player(1UL, "Pierre", "DURAND", "PIER", "avatar2"),
+                    new Player(2UL, "Paul", "MARTIN", "PAUL", "avatar3")),
             },
             new List<GroupEntity>()
             {
                 new GroupEntity
                 {
                     Id = 1UL,
-                    Name = "Group1"
+                    Name = "Group1",
+                    Players = new List<PlayerEntity>
+                    {
+                        new PlayerEntity
+                        { 
+                            Id = 0UL, 
+                            FirstName = "Jean",
+                            LastName = "BON",
+                            Nickname = "JEBO",
+                            Avatar = "avatar1"
+                        },
+                        new PlayerEntity
+                        {
+                            Id = 1UL,
+                            FirstName = "Pierre", 
+                            LastName = "DURAND",
+                            Nickname = "PIER", 
+                            Avatar = "avatar2"
+                        },
+                        new PlayerEntity
+                        {
+                            Id = 2UL,
+                            FirstName = "Paul",
+                            LastName = "MARTIN",
+                            Nickname = "PAUL",
+                            Avatar = "avatar3"
+                        }
+                    }
                 },
                 new GroupEntity
                 {
                     Id = 2UL,
-                    Name = "Group2"
+                    Name = "Group2",
+                    Players = new List<PlayerEntity>
+                    {
+                        new PlayerEntity
+                        { 
+                            Id = 0UL, 
+                            FirstName = "Jean",
+                            LastName = "BON",
+                            Nickname = "JEBO",
+                            Avatar = "avatar1"
+                        },
+                        new PlayerEntity
+                        {
+                            Id = 1UL,
+                            FirstName = "Pierre", 
+                            LastName = "DURAND",
+                            Nickname = "PIER", 
+                            Avatar = "avatar2"
+                        },
+                        new PlayerEntity
+                        {
+                            Id = 2UL,
+                            FirstName = "Paul",
+                            LastName = "MARTIN",
+                            Nickname = "PAUL",
+                            Avatar = "avatar3"
+                        }
+                    }
                 },
                 new GroupEntity
                 {
                     Id = 3UL,
-                    Name = "Group3"
+                    Name = "Group3",
+                    Players = new List<PlayerEntity>
+                    {
+                        new PlayerEntity
+                        { 
+                            Id = 0UL, 
+                            FirstName = "Jean",
+                            LastName = "BON",
+                            Nickname = "JEBO",
+                            Avatar = "avatar1"
+                        },
+                        new PlayerEntity
+                        {
+                            Id = 1UL,
+                            FirstName = "Pierre", 
+                            LastName = "DURAND",
+                            Nickname = "PIER", 
+                            Avatar = "avatar2"
+                        },
+                        new PlayerEntity
+                        {
+                            Id = 2UL,
+                            FirstName = "Paul",
+                            LastName = "MARTIN",
+                            Nickname = "PAUL",
+                            Avatar = "avatar3"
+                        }
+                    }
                 }
             }
         };
