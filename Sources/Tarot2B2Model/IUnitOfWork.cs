5ea62fc191e15d56e47e56d0b8579ae0ae637476
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-01-14 00:44:03 +0100

    :sparkles: finish unit of work and its interface
    IUnitOfWork and UnitOfWork has been implemented
    
    :warning: Class are not tested for now

--- Sources/Tarot2B2Model/IUnitOfWork.cs
+++ Sources/Tarot2B2Model/IUnitOfWork.cs
@@ -2,5 +2,22 @@
 
 internal interface IUnitOfWork : IDisposable
 {
+	/// <summary>
+	/// Return the repository for the specified type
+	/// </summary>
+	/// <typeparam name="TEntity"> The type of the entity </typeparam>
+	/// <returns> The repository </returns>
 	IGenericRepository<TEntity> Repository<TEntity>() where TEntity : class;
+	
+	/// <summary>
+	/// Save pending changes to the database
+	/// </summary>
+	/// <param name="cancellationToken"> The cancellation token </param>
+	/// <returns> The number of objects written to the underlying database </returns>
+	Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+	
+	/// <summary>
+	/// Reject pending changes to the database
+	/// </summary>
+	Task RejectChangesAsync();
 }
