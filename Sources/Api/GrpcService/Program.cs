0c8fa5f9194ca8f6da73a9e4fa3626b8405509b4
Samuel SIRVEN <samuel.sirven@etu.uca.fr>
2023-02-02 23:22:17 +0100

    :art: reformat grpc
    - Divide proto file by type
    - Add GroupService tp the GrpcMap
    - Add all proto files in Protos directory on the csproj

--- Sources/Api/GrpcService/Program.cs
+++ Sources/Api/GrpcService/Program.cs
@@ -28,7 +28,10 @@ builder.Services.AddTransient<IWriter, DbWriter>();
 
 var app = builder.Build();
 
+
 app.MapGrpcService<UserService>();
+app.MapGrpcService<GroupService>();
+
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
